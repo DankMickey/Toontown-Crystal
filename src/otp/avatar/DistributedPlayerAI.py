2d0cc9d4b75775afacb6b173032b60e4452571a0
Spiderlover <darthvader438@gmail.com>
2015-08-09 07:59:02 -0400

    Capitalized Crystal in Toontown Crystal

--- src/otp/avatar/DistributedPlayerAI.py
+++ src/otp/avatar/DistributedPlayerAI.py
@@ -131,7 +131,7 @@ def maintenance(minutes):
         dg = PyDatagram()
         dg.addServerHeader(10, simbase.air.ourChannel, CLIENTAGENT_EJECT)
         dg.addUint16(154)
-        dg.addString('Toontown crystal is now closed for maintenance.')
+        dg.addString('Toontown Crystal is now closed for maintenance.')
         simbase.air.send(dg)
         return Task.done
 
