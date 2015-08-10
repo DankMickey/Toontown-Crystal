76305c550ba8fa25aa0561c37b9995ed6bce3a15
Spiderlover <darthvader438@gmail.com>
2015-08-10 13:13:07 -0400

    Revert "Removed Disney References: Part 2"
    This reverts commit 7bcef03e1550c578ac009a60c88537553fce7122.

--- src/toontown/toonbase/ToontownGlobals.py
+++ src/toontown/toonbase/ToontownGlobals.py
@@ -148,14 +148,15 @@ def getSuitFont():
     return SuitFont
 
 
-CrystalDock = 1000
+DonaldsDock = 1000
 ToontownCentral = 2000
 TheBrrrgh = 3000
-Melodyland = 4000
-CrystalGarden = 5000
+MinniesMelodyland = 4000
+DaisyGardens = 5000
 OutdoorZone = 6000
-Speedway = 8000
-Dreamland = 9000
+FunnyFarm = 7000
+GoofySpeedway = 8000
+DonaldsDreamland = 9000
 ForestGrove = 10000
 BarnacleBoulevard = 1100
 SeaweedStreet = 1200
