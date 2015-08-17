e7084150b7b1a01fbd196d96b94ed9266e013aa6
craigy109 <crazycomet181@hotmail.com>
2015-08-17 21:14:02 +0100

    Added Pycrypto & Jsonrpc to the panda files, Also added easy installer for downloading python packages
    This is to get the rpc invasion working it still needs work on it

--- src/tools/rpc/rpc-invasions.bat
+++ src/tools/rpc/rpc-invasions.bat
@@ -1,5 +1,5 @@
 @echo off
 cd ../../../
 set /P serv=RPC Server:
-"C:\Panda3D-1.10.0\python\ppython.exe" "src/tools/rpc/rpc-invasions.py" 6163636f756e7473 %serv%
+"src/dependencies/panda/python/ppython.exe" "src/tools/rpc/rpc-invasions.py" 6163636f756e7473 %serv%
 pause
