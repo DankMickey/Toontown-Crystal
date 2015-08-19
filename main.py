dcd1c1f6d5c73e047b475fa9ccea1808f091e504
CodeAnGo <luke@codeango.com>
2015-08-19 14:04:01 +0100

    Needs fixing
    src\Main.py works perfectly
    but the other main.py doesn't work at all

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 import subprocess, os, sys
 
-#import src.toontown.toonbase.ToontownStart
-import src.toontown.toonbase
-
-username = raw_input("> ")
-password = raw_input("> ") 
-
-os.environ["TTCY_GAMESERVER"] = "server.toontowncrystal.com"
-os.environ["ttcyUsername"] = username
-os.environ["ttcyPassword"] = password
-
-subprocess.call(['src\dependencies\panda\python\ppython.exe', 'src/toontown/toonbase/ToontownStart.py'])
+subprocess.call(['src\dependencies\panda\python\ppython.exe', 'src\main.py'])
